af0977a4cd7b49577d2864309526ee49a045e93c
Alejandro Alcocer <ealcocer@kapital.com>
2025-08-01 11:52:40 -0600

    Se modifican varibles para el proyecto inicial

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function App() {
   return (
     <div>
-      <h1> Estamos en mi aplicación </h1>
+      <h1>Hola mundo desde mi aplicación</h1>
     </div>
   );
 }
